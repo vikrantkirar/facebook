18bafed968643dec36b00b203d9f72bfb0a5bd58
Vikrant Kirar <vikrantkirar@gmail.com>
2013-12-16 14:04:09 +0530

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Facebook::Application.config.secret_key_base = 'dda55cc885783e5332136e9c976ab651583d525a75c89bd59f407f48b7c64ebe23febe3711375f37cd19f4ceb035b586215891879115243f775b91f1a18a6a3b'
